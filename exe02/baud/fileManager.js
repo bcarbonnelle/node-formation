f660e9a06dc991441e4665746521623e774448cc
bcarbonnelle <b.carbonnelle@gmail.com>
2018-01-12 10:36:51 +0100

    modif fichier working

---
+++ exe02/baud/fileManager.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+let values=[]
+module.exports ={
+    init:(file,action,value)=>{
+        fs.readFile(file,(err,data)=>{
+            values = (data.toString().split(`\n`))
+            if(action===`add`){
+                add(value,file)
+            } else remove(value,file)
+        })
+    }
+}
+
+const add =(value,file) =>{
+    values.push(value)
+    save(file)
+}
+const remove =(value,file) =>{
+    const i = values.indexOf(value)
+    if(i<0) console.log(`value not found`)
+    else{
+        values.splice(i,1)
+    save(file)
+    }
+}
+const save = (file) =>{
+    let tempStr = values.reduce((prev,current)=>{
+        return `${prev}\n${current}`
+    })
+    fs.writeFile(file,tempStr,(err) =>{
+        if(err) console.log(err)
+        console.log(`File Saved`)
+    })
+}
--- exe02/server.js
+++ exe02/server.js
@@ -8,35 +8,58 @@
 // if(process.argv[2]='add'){
 //     fr.writeFile(`${process.cwd()}/liste.txt`, )
 // }
-const fs = require('fs')
-let content=''
-let value = process.argv[3]
-switch (process.argv[2]){
-    case 'add':
-        addNameInList(overwriteFile)
+
+const [action,value] = [process.argv[2],process.argv[3]]
+const possibleActions = ['add','remove']
+const fileMger = require(`./baud/fileManager`)
+const checkAction = (action) =>{
+    return possibleActions.filter(item => item ===action).length >0
+}
+const checkValue =(value) => (value) ? true : false // => retourne false si null ou undefined 0 ou NAN
+
+const init =() =>{
+    
+    if(!checkAction(action)){
+        console.log(`Error = the possible actions are :
+                                - add
+                                - remove`)
+    }
+    else if (!checkValue(value)){
+        console.log(`Error : You need to give value for execution!`)
+    }
+    else{
+        fileMger.init(`liste.txt`,action,value)
+    }
+}
+init()
+////////////////////////////premier code pas bon :(
+// let value = process.argv[3]
+// switch (process.argv[2]){
+//     case 'add':
+//         addNameInList(overwriteFile)
    
-        break
-    case 'remove':
-        break
-    default :
-        console.log(process.argv[2] + ' is not a valid command') 
-}
-function addNameInList(cb){
-    fs.readFile(`${process.cwd()}/liste.txt`,{encoding:'utf8'}, function(err,data){
-        if(err) console.log(err)
-        console.log('old content : ' + data)
-        data += '\n' + value
-        cb(data)
+//         break
+//     case 'remove':
+//         break
+//     default :
+//         console.log(process.argv[2] + ' is not a valid command') 
+// }
+// function addNameInList(cb){
+//     fs.readFile(`${process.cwd()}/liste.txt`,{encoding:'utf8'}, function(err,data){
+//         if(err) console.log(err)
+//         console.log('old content : ' + data)
+//         data += '\n' + value
+//         cb(data)
 
-    })
-}
-function
+//     })
+// }
 
-function overwriteFile(newcontent){
-    console.log('new content : ' + newcontent)
-    fs.writeFile(`${process.cwd()}/liste.txt`, newcontent,(err)=>{
-        if(err) console.log(err)
-        console.log('file correctly updated')
-    })
 
-}
+// function overwriteFile(newcontent){
+//     console.log('new content : ' + newcontent)
+//     fs.writeFile(`${process.cwd()}/liste.txt`, newcontent,(err)=>{
+//         if(err) console.log(err)
+//         console.log('file correctly updated')
+//     })
+
+// }
